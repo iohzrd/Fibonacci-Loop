30b626d04174f4901b874c1ee7bb7df4188fea5f
iohzrd <synergysymphony@gmail.com>
2015-06-06 11:40:17 -0700

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -2,20 +2,20 @@
 /* Fibonacci loop */
 main()
 {
-        unsigned int first, second, limit;
+        unsigned long long int first, second, limit;
 
         first = 1;
         second = 1;
         printf("Stop fibbin at #?\n");
-        scanf("%i", &limit);
+        scanf("%lli", &limit);
         while (first <= limit && second <= limit)
         {
-                printf("%i\n%i\n", first, second);
+                printf("%lli\n%lli\n", first, second);
                 first = first + second;
                 second = second + first;
         }
         if (first <= limit && second != limit)
-        printf("%i\n", first);
+        printf("%lli\n", first);
         else if (second <= limit && first != limit)
-        printf("%i\n", second);
+        printf("%lli\n", second);
 }
